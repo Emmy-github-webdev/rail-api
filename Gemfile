2954a66cfe7458b51bd6a44791bb34cc8a468c68
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-10 17:56:31 +0100

    make test for post model passes

--- Gemfile
+++ Gemfile
@@ -40,7 +40,7 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
-  gem 'rspec-rails', '~> 3.5'
+  gem 'rspec-rails', '~> 5.0.0'
 end
 
 group :development do
@@ -50,7 +50,7 @@ end
 
 group :test do
   gem 'factory_bot_rails', '~> 4.0'
-  gem 'shoulda-matchers', '~> 3.1'
+  gem 'shoulda-matchers', '~> 5.0'
   gem 'faker'
   gem 'database_cleaner'
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "test for post model" do
+    subject do
+      Post.new(
+        title: 'title',
+        text: 'text'
+      )
+    end
+  
+    it "should validate that attributes cannot be empty" do
+      expect(subject).to be_valid
+    end
+  
+    it "should validate that title cannot be empty" do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+  
+    it "should validate that text cannot be empty" do
+      subject.text = nil
+      expect(subject).to_not be_valid
+    end
+  end
+  
+  
 end
