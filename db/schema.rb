a793dd98faf532319febb1e74622bec5ee753ad8
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-10 19:12:44 +0100

    Generate user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_10_161554) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_10_181024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_10_161554) do
     t.text "text"
     t.integer "commentCounter"
     t.integer "likeCounter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
